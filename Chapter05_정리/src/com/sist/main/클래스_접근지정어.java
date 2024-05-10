befc4aa7281dcca3287750c2d51c7cbf73333b05
user <user@DESKTOP-HUGC3B7>
2024-05-10 10:43:36 +0900

    2024/05/10

---
+++ Chapter05_정리/src/com/sist/main/클래스_접근지정어.java
@@ -0,0 +1,17 @@
+package com.sist.main;
+/*	변수 / 메소드 / 생성자
+ *---------------------
+ *	접근 범위
+ *	=> private : 멤버변수 (데이터를 보호하는 목적) => 접근 거부
+ *	=> public : 다른 클래스 연결 			   => 접근 승낙
+ *				메소드 / 클래스 / 생성자
+ * 
+ */
+public class 클래스_접근지정어 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
